d8f0ed7a7ed888f10eb9e8d9ee7b86ff04bfafc7
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-12-18 11:33:16 -0600

    added rough draft of html and api routes

--- server.js
+++ server.js
@@ -14,3 +14,42 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+//Routes
+//HTML
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/home.html"));
+// });
+
+// app.get("/survey", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/survey.html"));
+// });
+
+// //API
+// app.get("/api/friends", function() {
+//   var chosen = req.params.friends;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//       for (var i = 0; i < character.length; i++) {
+//         if (chosen === friends[i].routeName) {
+//           return res.json(friends[i]);
+//         }
+//       }
+
+//       return res.json(false);
+//   }
+//   return res.json(friends);
+// });
+
+// app.post("/api/new", function(req, res) {
+//   var newFriend = req.body;
+//   newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+//   console.log(newFriend);
+
+//   friends.push(newFriend);
+
+//   res.json(newFriend);
+// })
+
